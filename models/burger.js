585a98f85a20b4e6e2950018850aef263edeeb25
liannaccone <lciannaccone@gmail.com>
2018-03-01 21:58:53 -0800

    adding code to the model that will call orm functions using specific inputs

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+// importing the ORM
+var orm = require('../config/orm.js');
+
+var burger = {
+	selectAll: function(cb) {
+		orm.selectAll('burgers', function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(cols, vals, cb) {
+		orm.insertOne('burgers', cols, vals, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function() {
+
+	}
+}
+
+// exporting the database functions for the controller (burgersController.js)...
+module.exports = burger;
