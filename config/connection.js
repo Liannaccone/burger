55bc0e6855a5ac0277419d87e02f3081c204d059
liannaccone <lciannaccone@gmail.com>
2018-03-05 16:27:06 -0800

    adding heroku connection for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 //  MySQL connection set up
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-});
-
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db",
+    });
+};
 // Establish connection...
 connection.connect(function(err) {
 	if (err) {
@@ -20,3 +26,4 @@ connection.connect(function(err) {
 
 // Export the connection for ORM to use...
 module.exports = connection;
+
